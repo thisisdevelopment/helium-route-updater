bfd49ba6a507b0901cb45d71b59f9368fdc98d20
Eric de Ruiter <eric@thisisdevelopment.nl>
2023-02-14 11:45:17 +0100

    [WIP] signing

--- pkg/updater/updater.go
+++ pkg/updater/updater.go
@@ -6,8 +6,27 @@ import (
 	"github.com/thisisdevelopment/helium-route-updater/pkg/listener"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/proto"
 	"log"
+	"reflect"
 )
+
+type SignedRequest interface {
+	proto.Message
+	GetSignature() []byte
+}
+
+func Sign[T SignedRequest](req T) T {
+	reflect.ValueOf(req).Elem().FieldByName("Signature").SetBytes([]byte{})
+
+	//TODO sign
+	//marshalled, _ := proto.Marshal(req)
+
+	signature := []byte{}
+	reflect.ValueOf(req).Elem().FieldByName("Signature").SetBytes(signature)
+
+	return req
+}
 
 func Run(server string, routeId string, ch <-chan listener.DeviceEvent) {
 	ctx := context.Background()
@@ -31,25 +50,26 @@ func Run(server string, routeId string, ch <-chan listener.DeviceEvent) {
 			AppEui:  event.AppEui,
 			DevEui:  event.DevEui,
 		}
+
 		if event.EventType == listener.EventType_Updated || event.EventType == listener.EventType_Deleted {
-			err := u.Send(&iot_config.RouteUpdateEuisReqV1{
+			err := u.Send(Sign(&iot_config.RouteUpdateEuisReqV1{
 				Action:    iot_config.ActionV1_remove,
 				EuiPair:   pair,
 				Timestamp: 0,
 				Signature: nil,
-			})
+			}))
 			if err != nil {
 				log.Fatal(err)
 				return
 			}
 		}
 		if event.EventType == listener.EventType_Updated || event.EventType == listener.EventType_Added {
-			err := u.Send(&iot_config.RouteUpdateEuisReqV1{
+			err := u.Send(Sign(&iot_config.RouteUpdateEuisReqV1{
 				Action:    iot_config.ActionV1_add,
 				EuiPair:   pair,
 				Timestamp: 0,
 				Signature: nil,
-			})
+			}))
 			if err != nil {
 				log.Fatal(err)
 				return
